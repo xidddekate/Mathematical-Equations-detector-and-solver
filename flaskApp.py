27231bfeb455a74699fafe326ee733cb2687d1fe
Sudhanshu Dekate <77233279+xidddekate@users.noreply.github.com>
2021-01-17 23:35:49 +0530

    Update flaskApp.py

--- flaskApp.py
+++ flaskApp.py
@@ -136,7 +136,7 @@ def predictBase64():
 
 
 def startServer():
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     # app.run(host='0.0.0.0', port=8080)
     app.run(host='127.0.0.1', port=5000)
 
